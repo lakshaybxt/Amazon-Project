6578cb0c1ec9ac2d076e7075ffd2d3b65ee679ce
lakshaybxt <bishtlakshay14@gmail.com>
2025-01-04 22:25:16 +0530

    switching button between update and save

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -38,9 +38,11 @@ cart.forEach((cartItem) => {
 							<span>
 								Quantity: <span class="quantity-label">${cartItem.quantity}</span>
 							</span>
-							<span class="update-quantity-link link-primary">
+							<span class="update-quantity-link link-primary js-update-link" 		data-product-id="${matchingProduct.id}">
 								Update
 							</span>
+							<input class="quantity-input">
+							<span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}" >Save</span>
 							<span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
 								Delete
 							</span>
@@ -120,3 +122,23 @@ document.querySelectorAll('.js-delete-link')
 	
 	updateCartQuantity();
 	
+	document.querySelectorAll('.js-update-link')
+		.forEach((link) => {
+			link.addEventListener('click', () => {
+				const productId = link.dataset.productId;
+				
+				const container = document.querySelector(`.js-cart-item-container-${productId}`);
+				container.classList.add('is-editing-quantity');
+			});
+		});
+
+		document.querySelectorAll('.js-save-link')
+			.forEach((link) => {
+				link.addEventListener('click', () => {
+					const productId = link.dataset.productId;
+
+					const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+					container.classList.remove('is-editing-quantity');
+				});
+			});
