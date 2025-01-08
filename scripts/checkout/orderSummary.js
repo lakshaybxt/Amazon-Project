82aa016112661d592be80072ee1e8b46f7efb9d4
lakshaybxt <bishtlakshay14@gmail.com>
2025-01-08 22:42:49 +0530

    create a function deliveryOptionsHTML to reuse the code

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliv
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
 import {renderCheckoutHeader} from './checkoutHeader.js';
 
@@ -18,12 +18,7 @@ export function renderOrderSummary() {
 
 		const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-		const today = dayjs();
-		const deliveryDate = today.add(
-			deliveryOption.deliveryDays,
-				'days'
-			);
-		const dateString = deliveryDate.format('dddd, MMMM D');
+		const dateString = calculateDeliveryDate(deliveryOption);
 
 		cartSummaryHTML += `
 				<div class="cart-item-container 
@@ -73,15 +68,10 @@ export function renderOrderSummary() {
 		let html = '';
 	;
 		deliveryOptions.forEach((deliveryOption) => {
-			const today = dayjs();
-			const deliveryDate = today.add(
-				deliveryOption.deliveryDays,
-				'days'
-				);
-				const dateString = deliveryDate.format('dddd, MMMM D');
-				const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
+			const dateString = calculateDeliveryDate(deliveryOption);
+			const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
-				const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+			const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
 			html +=`<div class="delivery-option js-delivery-option"
 			data-product-id="${matchingProduct.id}"
