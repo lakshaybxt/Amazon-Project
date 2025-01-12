e6161678a50662d4c49e646053068919bc81205b
lakshaybxt <bishtlakshay14@gmail.com>
2025-01-12 21:32:38 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -93,6 +93,8 @@ export function loadProductsFetch() {
     });
   
     console.log('load products');
+  // }).catch((error) => {
+  //   console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -121,10 +123,14 @@ export function loadProducts(fun) {
   fun();
 });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.')
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-// loadProducts();
+
 
 /*
 export const products = [
